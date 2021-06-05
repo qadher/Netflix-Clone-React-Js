172198b6157c9f6ce92fdc77eb8fa97885e22ea2
qadher <qadher7@gmail.com>
2021-06-05 22:16:22 +0530

    navbar finish

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import '../Components./Nav.css'
+import React, { useEffect, useState } from 'react'
+import '../Components/Nav.css'
 
 function Nav() {
+
+const [show, handleshow] = useState(false);
+
+const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+        handleshow(true)
+    } else{
+        handleshow(false)
+    }
+}
+
+useEffect(() => {
+
+    window.addEventListener("scroll", transitionNavBar)
+    return () => window.removeEventListener('scroll', transitionNavBar)
+}, [])
+
+
+
     return (
-        <div className='nav'>
+        <div className={`nav ${show && 'nav_black'}`}>
+
+           <div className="nav_contents">
+
+           <img 
+            className="nav_logo"
+            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="Netflix-logo"/>
             
+            <img 
+            className="nav_avatar"
+            src="https://1.bp.blogspot.com/-4vqOLs-4Wbc/YLuaMiX2jsI/AAAAAAAAAAM/kDLfziwTx7QSofPb4VjiQR8-zspDQDiuwCNcBGAsYHQ/s320/profile_avatar.png" alt="Netflix-logo"/>
+
+            
+
+           </div>
+
         </div>
     )
 }
 
 export default Nav
+
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,9 +1,77 @@
 import React from 'react'
+import Nav from './Components/Nav'
 import './HomeScreen.css'
 
 function HomeScreen() {
     return (
         <div className="homeScreen">
+
+            <Nav />
+
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
             
         </div>
     )
