9bbd63614525b7144f8c593d1e144d795e708227
qadher <qadher7@gmail.com>
2021-06-09 00:07:36 +0530

    nav and banner complete - description error

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,50 +1,57 @@
-import React from 'react'
+import axios from '../Api/axios';
+import React, { useEffect, useState } from 'react'
+import requests from '../Api/Requests';
 import '../Components/Banner.css'
+
 
 function Banner() {
 
-      function truncate(string, n) {
-        return string?.lenght > n ? string.substr(0, n - 1) + '...' : string;
-      }
+  const [movie, setmovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setmovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      )
+     return request;
+    }
+    
+    fetchData();
+  }, [])
+
+  console.log(movie);
+
+
+    function truncate(string, n) {
+      return string?.lenght > n ? string.substr(0, n - 1) + '...' : string;
+    }
+  
 
     return (
         <header className="banner"
           style={{
             backgroundSize: "cover",
-            backgroundImage: `url("https://wallpapercave.com/wp/wp2831956.png")`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: "center center"
     
           }}
         
         >
             <div className="banner_contents">
-                 <h1 className="banner_title">Movie Name</h1>
+                 <h1 className="banner_title">
+                   {movie?.title || movie?.name || movie?.original_name}
+                 </h1>
                 <div className="banner_buttons">
                   <button className="banner_button">Play</button>
                   <button className="banner_button">My list</button>
 
                 </div>
                 <h1 className="banner_description">
-                  {truncate(
-                    `This is a test description K-12 education is of great 
-                    importance due to its effect on the development of future 
-                    citizens.  With the advance of technology, scientific literacy 
-                    is becoming essential for adequate participation in our 
-                    society.  There is concern that the U.S. is falling behind 
-                    other countries in science education, especially at the K-8
-                    grade levels.  There is also a common attitude among 
-                    students that science is too hard, too boring, and not worth
-                     the effort.  Teachers have a great deal of difficulty 
-                     preventing the onset of this attitude, and little hope of
-                      reversing it once it takes hold. This is partly due to a 
-                      socialization which places little regard for, or importance,
-                       on scholarship in science and math.  If significant progress 
-                       in scholarship is to be made in these areas, it must be at the
-                        expense of preconceived notions of unimportance and difficulty.
-                          However, limitations in the training pre-service teachers are 
-                          given to teach science is also to blame.`, 
-                  150
-                  )}
+                {truncate(
+                    movie?.overview, 150)}
                   </h1>
 
             </div>
