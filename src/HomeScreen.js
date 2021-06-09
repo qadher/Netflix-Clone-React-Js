030557066f3fcb4294a50ecf9d8f15e7344c0e74
qadher <qadher7@gmail.com>
2021-06-09 11:23:54 +0530

    Homepage Completed

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Banner from './Components/Banner'
 import Nav from './Components/Nav'
 import './HomeScreen.css'
+import requests from './Api/Requests'
+import Row from './Components/Row'
 
 function HomeScreen() {
     return (
@@ -10,6 +12,19 @@ function HomeScreen() {
             <Nav />
 
            <Banner />
+
+           <Row 
+             title='NETFLIX ORIGINALS'
+             fetchUrl={requests.fetchNetflixOriginals}
+             isLargeRow
+           />
+            <Row title='Tending Now' fetchUrl={requests.fetchTrending} />
+            <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+            <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+            <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+            <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+            <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+            <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
             
         </div>
     )
